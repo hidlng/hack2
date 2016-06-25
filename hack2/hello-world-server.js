e98f5cfa08e3f91370d2eb0b887492cc876cfba2
kang <kang@kang-PC>
2016-06-26 04:48:28 +0900

    123

--- hack2/hello-world-server.js
+++ hack2/hello-world-server.js
@@ -1,35 +1,8 @@
-
+var http = require('http');
 var status = "";
 var id = "";
 var img_cctv="";
 var helmet = "";
-
-http = require('http');
-fs = require('fs');
-server = http.createServer( function(req, res) {
-
-    console.dir(req.param);
-
-    if (req.method == 'POST') {
-        console.log("POST");
-        var body = '';
-        req.on('data', function (data) {
-            body += data;
-            console.log("Partial body: " + body);
-        });
-        req.on('end', function () {
-            console.log("Body: " + body);
-        });
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('post received');
-    }
-});
-
-port = 8080;
-host = '192.168.43.26';
-server.listen(port, host);
-console.log('Listening at http://' + host + ':' + port);
-
 /*
 var Gpio = require('onoff').Gpio,    
 //GPIO 21번을 출력으로 사용할 수 있는 객체를 생성  
@@ -59,3 +32,8 @@ led.writeSync(0);  // LED를 끈다.
 led.unexport();    // 사용했던 GPIO자원을 해제한다.   
 }, 5000); */
 
+http.createServer(function (req, res) {
+	 res.on('data', function(chunk){
+	        console.log("body: " + chunk);
+	    });
+}).listen(8080);
