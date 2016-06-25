cafeaf394f6fcc894c026a755f8eeced8fb05a20
kang <kang@kang-PC>
2016-06-26 02:53:01 +0900

    111

--- hack2/hello-world-server.js
+++ hack2/hello-world-server.js
@@ -9,36 +9,21 @@
 	      baudrate: 115200
 	    });
 	    serialPort.on("open", function (data) {
-	    	console.log('data : ' + data + 'indexof : ' + data.indexOf("panic")  + 'indexof : ' + data.indexOf("fall")  + 'indexof : ' + data.indexOf("wakeup"));
-	      serialPort.on('data', function(data) {
-	    	 /* 
-	    	 request({
-				  uri: "http://52.79.138.81/saint/worker/update?id="+id+"&status="+status+"&img_cctv="+img_cctv+"&helmet="+helmet,
-				  method: "GET"
-				  }, function(error, response, body) {
-					 console.log(response); 
-				});
-	    	 */
-	    	  console.log('data : ' + data + 'indexof : ' + data.indexOf("panic")  + 'indexof : ' + data.indexOf("fall")  + 'indexof : ' + data.indexOf("wakeup")); 
-    		  if( data.indexOf("panic") > 0 ) {
-    			  console.log('data : ' + data); 
-    		  }
+	      console.log('data : ' + data);
+  		  if( data.indexOf("panic") > 0 ) {
+			  console.log('data : ' + data); 
+		  }
 
-    		  if( data.indexOf("fall") > 0 ) {
-    			  console.log('data : ' + data); 
-    		  }
+		  if( data.indexOf("fall") > 0 ) {
+			  console.log('data : ' + data); 
+		  }
 
-    		  
-    		  if( data.indexOf("wakeup") > 0 ) {
-    			  console.log('data : ' + data); 
-    		  }
-
-	    	  
-	    	  
-	    	 
-	    		  
-	      });
-	      
+		  
+		  if( data.indexOf("wakeup") > 0 ) {
+			  console.log('data : ' + data); 
+		  }
+	    	
+	    	
 	      serialPort.write(new Buffer('10','ascii'), function(err, results) {
 	        console.log('err ' + err);
 	        console.log('results ' + results);
