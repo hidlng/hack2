7f8416e91790812ab5ab680782de14e6991ab788
kang <kang@kang-PC>
2016-06-25 17:55:00 +0900

    111

--- hack2/hello-world-server.js
+++ hack2/hello-world-server.js
@@ -1,6 +1,13 @@
-var http = require('http');
-http.createServer(function handler(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+   var request = require("request");
+   var status = "";
+   var id = "";
+   var img_cctv="";
+   var helmet = "";
+
+  
+   request({
+		  uri: "http://52.79.138.81/saint/worker/update?id="+id+"&status="+status+"&img_cctv="+img_cctv+"&helmet="+helmet,
+	  method: "GET"
+	  }, function(error, response, body) {
+		 console.log(response); 
+	});
