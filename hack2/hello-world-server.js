7547e373b823097aba7b14802cabcc21bbfe3ee1
kang <kang@kang-PC>
2016-06-26 02:55:49 +0900

    123

--- hack2/hello-world-server.js
+++ hack2/hello-world-server.js
@@ -10,7 +10,64 @@
 	    });
 	    serialPort.on("open", function (data) {
 	      console.log('data : ' + data);
+/*
+	      if( data.indexOf("panic") > 0 ) {
+			  console.log('data : ' + data); 
+		  }
 
+		  if( data.indexOf("fall") > 0 ) {
+			  console.log('data : ' + data); 
+		  }
+
+		  
+		  if( data.indexOf("wakeup") > 0 ) {
+			  console.log('data : ' + data); 
+		  }
+	*/    
+	      serialPort.on('data', function(data) {
+	    	  console.log('data received: ' + data); 
+	    	 /* 
+	    	 request({
+				  uri: "http://52.79.138.81/saint/worker/update?id="+id+"&status="+status+"&img_cctv="+img_cctv+"&helmet="+helmet,
+				  method: "GET"
+				  }, function(error, response, body) {
+					 console.log(response); 
+				});
+	    	 
+	    	  if( status.replace(/ /gi, "").replace(/\n/gi, "")  == "+panic" ) {
+	    		  console.log('data received: ' + status);  
+	    		  status = "";
+	    	  } else {
+	    		  if( status.indexOf(data) < 0 ) {
+	    			  status += data;
+	    			  console.log('data yet: ' + status); 
+	    		  }
+	    	  }
+
+	    	  if( status.replace(/ /gi, "").replace(/\n/gi, "") == "+fall" ) {
+	    		  console.log('data received: ' + status);  
+	    		  status = "";
+	    	  } else {
+	    		  if( status.indexOf(data) < 0 ) {
+	    			  status += data;
+	    			  console.log('data yet: ' + status); 
+	    		  }
+	    	  }
+	    	  
+	    	  if( status.replace(/ /gi, "").replace(/\n/gi, "") == "+rise" ) {
+	    		  console.log('data received: ' + status);  
+	    		  status = "";
+	    	  } else {
+	    		  if( status.indexOf(data) < 0 ) {
+	    			  status += data;
+	    			  console.log('data yet: ' + status); 
+	    		  }
+	    	  }
+	    	  
+	    	 */
+	    		  
+	      });
+	      
 	    	
 	      serialPort.write(new Buffer('10','ascii'), function(err, results) {
 	        console.log('err ' + err);
